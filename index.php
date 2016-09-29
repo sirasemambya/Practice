64c1c267bc44bfecca8999cb49ad250a2f96bde4
sira b semambya <sbs43@afsconnect2.njit.edu>
2016-09-29 13:49:02 -0400

    chnages made

--- index.php
+++ index.php
@@ -8,5 +8,17 @@
 	echo '$myvar';
 	echo '<br>';
 	 "$myvar";
+//this is an exmple of php arrays
+
+$myarray = array ();
+$myarray[] = 'some value 1';
+$myarray[] = 'some value 2';
+$myarray[] = 'some value 3';
+
+print_r($myarray);
+
+$myAssoc = array('value1' => $myarray, 'value2' => $myarray);
+
+print_r($myAssoc);
 
 ?>
