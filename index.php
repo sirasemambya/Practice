a92817c94163d2bcbc8224d5bd70291297d3d69d
sira b semambya <sbs43@afsconnect2.njit.edu>
2016-09-29 13:35:33 -0400

    added new code

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+	//this is how you print text in php
+	echo 'hello world';
+	//this is how you store a variable
+	$myvar = 'some text i want to store in a variable';
+	//difference between single and double quotes
+	 echo '<br>';
+	echo '$myvar';
+	echo '<br>';
+	 "$myvar";
 
-echo 'hello world';
 ?>
